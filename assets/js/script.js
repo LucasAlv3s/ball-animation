d38ff71fdbece54c6553a71f11bf1e40b62be815
Lucas Alves <80328704+LucasAlv3s@users.noreply.github.com>
2023-01-27 19:36:21 -0300

    desenvolvendo o ball animation

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    let NBALL = 100;
+    let R = null;
+    let TIME_INTERVAL = 20;
+    let BACK_ALPHA = 0.1;
+
+    // canvas API
+    let canvas = document.getElementById('mycanvas');
+    let ctx = canvas.getContext('2d');
+    ctx.canvas.width = 640;
+    ctx.canvas.height = 480;
+
+    // Draw
+    ctx.fillStyle = 'red';
+    ctx.beginPath();
+    ctx.arc(ctx.canvas.window / 2, ctx.canvas.height / 2, 100.0, 0.0, Math.);
+}
